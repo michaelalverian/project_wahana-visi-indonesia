9ba56cebeba67cdfbfbc28eab7ec47051efa94aa
michaelalverian <michael.alverian@gmail.com>
2023-08-01 11:32:36 +0700

    changing url from static to django based, delete kader in correspondence

--- wvi/urls.py
+++ wvi/urls.py
@@ -9,36 +9,36 @@ from wvi.views.participant_views import ParticipantView
 
 
 urlpatterns = [
-    path('', CustomAuthentication.loginView, name='login'),
+    path("", CustomAuthentication.loginView, name='login'),
     
     # Authentication
-    path('login/', CustomAuthentication.loginView, name='login'),
-    path('logout/', CustomAuthentication.logoutView, name='logout'),
-    path('signup/', CustomAuthentication.signupView, name='signup'),
+    path("login/", CustomAuthentication.loginView, name='login'),
+    path("logout/", CustomAuthentication.logoutView, name='logout'),
+    path("signup/", CustomAuthentication.signupView, name='signup'),
 
     # Child
     path("child", ChildView.view, name="child"),
-    path("child/update", ChildView.update),
+    path("child/update", ChildView.update, name='child_update'),
 
     # Child Record
     path("childrecord", ChildRecordView.view, name="childrecord"),
-    path("childrecord/insert_excel", ChildRecordView.insert_excel),
-    path("childrecord/update", ChildRecordView.update),
-    path("childrecord/update_target", ChildRecordView.update_target),
+    path("childrecord/insert_excel", ChildRecordView.insert_excel, name='childrecord_insert_excel'),
+    path("childrecord/update", ChildRecordView.update, name='childrecord_update'),
+    path("childrecord/update_target", ChildRecordView.update_target, name='childrecord_update_target'),
 
     # Kader
-    path("kader", KaderView.view, name="kader"),
-    path("kader/insert", KaderView.insert),
-    path("kader/update", KaderView.update),
-    path("kader/delete", KaderView.delete),
+    path("kader", KaderView.view, name='kader'),
+    path("kader/insert", KaderView.insert, name='kader_insert'),
+    path("kader/update", KaderView.update, name='kader_update'),
+    path("kader/delete", KaderView.delete, name='kader_delete'),
 
     # Correspondence
     path("correspondence", CorrespondenceView.view, name="correspondence"),
-    path("correspondence/insert_excel", CorrespondenceView.insert_excel),
-    path("correspondence/update_kader", CorrespondenceView.update_kader),
-    path("correspondence/update_due_date", CorrespondenceView.update_due_date),
+    path("correspondence/insert_excel", CorrespondenceView.insert_excel, name='correspondence_insert_excel'),
+    path("correspondence/update_kader", CorrespondenceView.update_kader, name='correspondence_update_kader'),
+    path("correspondence/update_due_date", CorrespondenceView.update_due_date, name='correspondence_update_due_date'),
 
     # Participant
-    path("participant", ParticipantView.view, name="participant"),
-    path("participant/insert_excel", ParticipantView.insert_excel, name="participant_insert_excel"),
+    path("participant", ParticipantView.view, name='participant'),
+    path("participant/insert_excel", ParticipantView.insert_excel, name='participant_insert_excel'),
 ]
